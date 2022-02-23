42f14d09923cb8c79bbbbb52464e16d05c3d216e
souravdb <souravd.now@gmail.com>
2022-02-23 18:35:23 -0500

    corrected ids

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,8 +16,8 @@ const Header = () => {
         <div className="container mx-auto px-10 mb-8">
             <div className="border-b w-full inline-block border-blue-400 py-8">
                 <div className="float-right justify-evenly">
-                    <Link id="menu-about" href="/about" passHref>
-                        <span className="md:float-right mt-2 align-middle text-sm text-white hover:text-amber-300 ml-4 px-4 font-semibold cursor-pointer">
+                    <Link href="/about" passHref>
+                        <span id="menu-about" className="md:float-right mt-2 align-middle text-sm text-white hover:text-amber-300 ml-4 px-4 font-semibold cursor-pointer">
                             About
                         </span>
                     </Link>
@@ -25,8 +25,8 @@ const Header = () => {
 
                 <div className="float-right justify-evenly">
                     {categories.map((category, index) => (
-                        <Link id={`menu-${index}`} key={index} href={`/category/${category.slug}`} passHref>
-                            <span className="md:float-right mt-2 align-middle text-sm text-white hover:text-amber-300 ml-4 px-4 font-semibold cursor-pointer">
+                        <Link key={index} href={`/category/${category.slug}`} passHref>
+                            <span id={`menu-${index}`} className="md:float-right mt-2 align-middle text-sm text-white hover:text-amber-300 ml-4 px-4 font-semibold cursor-pointer">
                                 {category.name}
                             </span>
                         </Link>
