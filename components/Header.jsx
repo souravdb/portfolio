41ee7278efb95d25697e028117549de70b58432d
souravdb <souravd.now@gmail.com>
2022-03-01 21:49:37 -0500

    adjust menu for small screen

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,6 +5,7 @@ import { getCategories } from '../services'
 
 const Header = () => {
     const [categories, setCategories] = useState([])
+    const [isMenuVisible, setMenuVisibility] = useState(false);
 
     useEffect(() => {
         getCategories().then((result) => {
@@ -15,34 +16,51 @@ const Header = () => {
     return (
         <div className="container mx-auto px-10 mb-8">
             <div className="border-b w-full inline-block border-blue-400 py-8">
-                <div className="float-right justify-evenly">
+
+                {/* invisible beyond small */}
+                <div className="visible sm:invisible float-right justify-evenly">
+                    <button
+                        onClick={() => setMenuVisibility(!isMenuVisible)}
+                        data-cy="mmenu-btn"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-4 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                            <title>Menu</title>
+                        </svg>
+                    </button>
+                </div>
+                <div className={`${isMenuVisible ? 'max-h-full' : 'h-0'} overflow-hidden`}>
+                    {categories.map((category, index) => (
+                        <Link key={index} href={`/category/${category.slug}`} passHref>
+                            <span id={`menu-${index}`} className="block sm:invisible mt-2 align-middle text-sm text-white ml-4 font-semibold cursor-pointer">
+                                {category.name}
+                            </span>
+                        </Link>
+                    ))}
+
                     <Link href="/" passHref>
-                        <span id="menu-about" className="md:float-right mt-2 align-middle text-sm text-amber-300 ml-4 px-4 font-semibold cursor-pointer">
+                        <span id="menu-about" className="block sm:invisible mt-2 align-middle text-sm text-white ml-4 font-semibold cursor-pointer">
                             About
                         </span>
                     </Link>
                 </div>
 
-                <div className="float-right justify-evenly">
+
+                {/* visible beyond small */}
+                <div className="invisible sm:visible float-right justify-evenly">
                     {categories.map((category, index) => (
                         <Link key={index} href={`/category/${category.slug}`} passHref>
-                            <span id={`menu-${index}`} className={`md:float-right mt-2 align-middle text-sm text-white ml-4 px-4 font-semibold cursor-pointer`}>
-                                {/* {(() => {
-                                    switch (category.name) {
-                                        case 'Engineering':
-                                            return <span id={`dot-${index}`} className="w-4 h-4 bg-amber-300 inline-block rounded-full mr-2"></span>
-                                        case 'Leadership':
-                                            return <span id={`dot-${index}`} className="w-4 h-4 bg-red-700 inline-block rounded-full mr-2"></span>
-                                        case 'Consulting':
-                                            return <span id={`dot-${index}`} className="w-4 h-4 bg-cyan-300 inline-block rounded-full mr-2"></span>
-                                        default:
-                                            return <span id={`dot-${index}`} className="w-4 h-4 bg-white inline-block rounded-full mr-2"></span>
-                                    }
-                                })()} */}
+                            <span id={`menu-${index}`} className="mt-2 align-middle text-sm text-white ml-4 px-4 font-semibold cursor-pointer">
                                 {category.name}
                             </span>
                         </Link>
                     ))}
+
+                    <Link href="/" passHref>
+                        <span id="menu-about" className="mt-2 align-middle text-sm text-white ml-4 px-4 font-semibold cursor-pointer">
+                            About
+                        </span>
+                    </Link>
                 </div>
             </div>
         </div>
